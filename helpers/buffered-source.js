f51493b70d3f381b8b8220ae43dacf157001e7de
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2018-10-11 11:45:34 -0700

    helpers: cleanup

--- helpers/buffered-source.js
+++ helpers/buffered-source.js
@@ -5,75 +5,69 @@ const EventEmitter = require('events')
 const status_type = require('bob-status')
 
 class BufferedSource extends EventEmitter {
-    constructor() {
-        super()
-        this.sink = null
+  constructor() {
+    super()
+    this.sink = null
 
-        // TODO: implement resize reallocation
-        this.buffer = Buffer.alloc(2048)
+    // TODO: implement resize reallocation
+    this.buffer = Buffer.alloc(2048)
 
-        this.bytesToWrite = 0
-        this.writePos = 0
-        this.ended = false
+    this.bytesToWrite = 0
+    this.writePos = 0
+    this.ended = false
+  }
+
+  bindSink(sink) {
+    this.sink = sink
+  }
+
+  pull(error, buffer) {
+    if (error) {
+      return this.sink.next(status_type.error, error)
     }
 
-    bindSink(sink) {
-        this.sink = sink
+    if (this.ended) {
+      return this.sink.next(status_type.end, null, buffer, 0)
     }
 
-    pull(error, buffer) {
-        console.log('bs got pull')
-        if (error) {
-            return this.sink.next(status_type.error, error)
-        }
-
-        if (this.ended) {
-            console.log('ending')
-            return this.sink.next(status_type.end, null, buffer, 0)
-        }
-
-        if (!this.bytesToWrite) {
-            return this.once('send', _ => {
-                console.log('delayed pulling')
-                this.pull(error, buffer)
-            })
-        }
-
-        const bytesWritten = this.buffer.copy(buffer, 0, 0, this.bytesToWrite)
-        this.bytesToWrite -= bytesWritten
-
-        console.log('writing')
-        console.log(buffer.slice(0, bytesWritten).toString())
-
-        this.sink.next(status_type.continue, null, buffer, bytesWritten)
-
-        this.buffer.copy(this.buffer, 0, bytesWritten, this.writePos)
-        this.writePos -= bytesWritten
+    if (!this.bytesToWrite) {
+      return this.once('send', _ => {
+        this.pull(error, buffer)
+      })
     }
 
-    send(buffer) {
-        if (typeof buffer === 'string') {
-            buffer = Buffer.from(buffer)
-        }
-        if (!Buffer.isBuffer(buffer)) {
-            throw new TypeError('send()\'s argument must be a String or Buffer!')
-        }
+    const bytesWritten = this.buffer.copy(buffer, 0, 0, this.bytesToWrite)
+    this.bytesToWrite -= bytesWritten
 
-        if (this.ended) {
-          throw new Error('Messages cannot be send after end()!')
-        }
+    this.sink.next(status_type.continue, null, buffer, bytesWritten)
 
-        buffer.copy(this.buffer, this.writePos)
-        this.bytesToWrite += buffer.length
-        this.writePos += buffer.length
+    this.buffer.copy(this.buffer, 0, bytesWritten, this.writePos)
+    this.writePos -= bytesWritten
+  }
 
-        this.emit('send')
+  send(buffer) {
+    if (typeof buffer === 'string') {
+      buffer = Buffer.from(buffer)
+    }
+    if (!Buffer.isBuffer(buffer)) {
+      throw new TypeError('send()\'s argument must be a String or Buffer!')
     }
 
-    end() {
-      this.ended = true
-      this.emit('send')
+    if (this.ended) {
+      throw new Error('Messages cannot be send after end()!')
     }
+
+    buffer.copy(this.buffer, this.writePos)
+    this.bytesToWrite += buffer.length
+    this.writePos += buffer.length
+
+    this.emit('send')
+  }
+
+  end() {
+    this.ended = true
+    this.emit('send')
+  }
 }
 
 module.exports = BufferedSource
--- helpers/stdout-sink.js
+++ helpers/stdout-sink.js
@@ -4,40 +4,40 @@ const { Buffer } = require('buffer')
 const status_type = require('bob-status')
 
 class StdoutSink {
-    constructor() {
-        this.source = null
-        this.bindCb = null
+  constructor(opts = {}) {
+    this.source = null
+    this.bindCb = null
 
-        try {
-            this._buffer = Buffer.allocUnsafe(64 * 1024)
-        } catch (error) {
-            this._allocError = error
-        }
+    try {
+      this._buffer = Buffer.allocUnsafe(64 * 1024)
+    } catch (error) {
+      this._allocError = error
+    }
+  }
+
+  bindSource(source, bindCb) {
+    if (this._allocError) {
+      return this.bindCb(this._allocError)
     }
 
-    bindSource(source, bindCb) {
-        if (this._allocError) {
-            return this.bindCb(this._allocError)
-        }
+    this.source = source
+    this.bindCb = bindCb
 
-        this.source = source
-        this.bindCb = bindCb
+    this.source.bindSink(this)
+  }
 
-        this.source.bindSink(this)
-    }
+  next(status, error, buffer, bytes) {
+    if (status === status_type.end) return this.bindCb()
+    if (error) return this.bindCb(error)
 
-    next(status, error, buffer, bytes) {
-        if (status === status_type.end) return this.bindCb()
-        if (error) return this.bindCb(error)
+    process.stdout.write(buffer.slice(0, bytes))
 
-        process.stdout.write(buffer.slice(0, bytes))
+    this.source.pull(null, this._buffer)
+  }
 
-        this.source.pull(null, this._buffer)
-    }
-
-    start() {
-        this.source.pull(null, this._buffer)
-    }
+  start() {
+    this.source.pull(null, this._buffer)
+  }
 }
 
 module.exports = StdoutSink
